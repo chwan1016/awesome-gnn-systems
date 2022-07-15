3070a47b63d19d0eec1aff316eb94ac84f0eacc9
Cheng Wan <chwan@rice.edu>
2022-07-15 11:58:03 -0700

    fix error detection

--- src/update_citation.py
+++ src/update_citation.py
@@ -12,6 +12,9 @@ def get_citation(name):
 	page = requests.get(scholar_link).text
 	soup = BeautifulSoup(page, "html.parser")
 
+	if "robot" in page:
+		raise ConnectionRefusedError
+
 	cite = soup.find_all("a", href=lambda value: value and value.startswith("/scholar?cites="))
 	s = cite[0].text if len(cite) >= 1 else "0"
 	s = int(''.join(c for c in s if c.isdigit()))
@@ -27,11 +30,17 @@ today = datetime.date.today()
 
 for item in sorted_table[0:3]:
 	name = item[0]
-	cite = get_citation(name)
-	item[1]['citation'] = cite
-	item[1]['last update'] = today.strftime("%Y-%m-%d")
+	try:
+		cite = get_citation(name)
+		item[1]['citation'] = cite
+		item[1]['last update'] = today.strftime("%Y-%m-%d")
+		print('"' + name + '" updated')
+	except ConnectionRefusedError:
+		print('updating "' + name + '" failed')
+		break
 
 table = dict(sorted_table)
 
 with open('../.github/citation/citation.json', 'w') as f:
 	json.dump(table, f)
+
